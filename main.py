c077ec5768a7fefb92f0c82240c830f25f0dfd8c
bhandekunal16 <bhandekunal16@gmail.com>
2023-09-26 12:40:39 +0530

    update

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template,jsonify, Request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
 @app.route('/greet/<name>')
 def greet(name):
     return f'Hello, {name}!'
 
+
+@app.route('/handle_click', methods=['POST'])
+def handle_click():
+    new_content = "Content updated after clicking the button."
+    return new_content
+
+
 @app.route('/index')
 def index():
-    title = "Title"
-    message = "This content was rendered on the server side using Flask and Jinja2."
-    
-    html_template = """
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Server-Side Rendering Example</title>
-    </head>
-    <body>
-        <h1 style="text-align : center;">{{ title }}</h1>
-        <p>{{ message }}</p>
-    </body>
-    </html>
-    """
-    
-    return render_template_string(html_template, title=title, message=message)
+    title = "resume"
+    content= handle_click()
+    message = "my name is kunal and i am software developer"
+    name = "kuanl"
+    print(content)
+    return render_template('home.html', title=title, message=message, name=name, content=content)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
