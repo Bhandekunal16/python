fd29b999e8f3c8f1f305eafaa4468239d6493ab9
bhandekunal16 <bhandekunal16@gmail.com>
2023-09-27 13:01:47 +0530

    update

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+from neo4j import GraphDatabase
+import neo4j
+from retrying import retry
 
 app = Flask(__name__)
+uri = "neo4j+s://b76e3d84.databases.neo4j.io:7687"
+user = "neo4j"
+password = "kH8WQkwu-vK5bmjUYjJ2oe1kbcBeoZdDeErj9o8woSk"
+
+
+class Neo4jDatabase:
+    def __init__(self, uri, user, password):
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self._driver.close()
+
+    def create_node(self, name, email, profession):
+        with self._driver.session() as session:
+            result = session.write_transaction(
+                self._create_node, name, email, profession)
+        return result
+
+    @staticmethod
+    def _create_node(tx, name, email, profession):
+        query = (
+            "CREATE (n:Person {name: $name, email: $email, profession: $profession})"
+            "RETURN id(n)"
+        )
+        result = tx.run(query, name=name, email=email, profession=profession)
+        return result.single()[0]
+
+
+# Retry 3 times with a 1-second delay between retries
+@retry(stop_max_attempt_number=3, wait_fixed=1000)
+def connect_to_neo4j():
+    # Your code to connect to Neo4j here
+    pass
+
+
+try:
+    connect_to_neo4j()
+except neo4j.exceptions.ServiceUnavailable:
+    print("Failed to connect to Neo4j after multiple retries.")
 
 
 @app.route('/')
@@ -38,9 +80,12 @@ def default():
     title = "personal assistant"
     print(process_input())
     user_input, name, profession = process_input()
+    db = Neo4jDatabase(uri, user, password)
+    content = db.create_node(name, user_input, profession)
     content = handle_click(name)
     message = f"my name is {name} and i am {profession} and my email Id is {user_input}"
     print(content)
+    db.close()
     return render_template('assistant.html', title=title, message=message, content=content)
 
 
